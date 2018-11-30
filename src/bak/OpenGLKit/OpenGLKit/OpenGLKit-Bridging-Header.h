e4dec7d1f4b39fe4c4484b82750ddc5f6095de5c
Liang Zhinian <a13533550310@live.com>
2018-12-01 00:05:03 +0800

    bug fix

---
+++ src/bak/OpenGLKit/OpenGLKit/OpenGLKit-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  DLTriangle-Bridging-Header.h
+//  OpenGLKit
+//
+//  Created by sprite on 2018/11/23.
+//  Copyright © 2018年 Ray Wenderlich. All rights reserved.
+//
+
+#ifndef GEOMETRY_Bridging_Header_h
+#define GEOMETRY_Bridging_Header_h
+
+//#import "Geometry.h"
+
+
+#endif /* GEOMETRY_Bridging_Header_h */
